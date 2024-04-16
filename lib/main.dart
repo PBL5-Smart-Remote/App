9f6499ff0768ddb5a457a5acd82dbddae3ffa4c9
huyycp <baohuyvongoc1@gmail.com>
2024-04-17 00:32:42 +0700

    add account page

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    isValidToken = true;
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<DeviceViewModel>(create: (_) => DeviceViewModel()),
--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -1,3 +1,4 @@
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:smart_home_fe/pages/generic_page.dart';
@@ -17,12 +18,62 @@ class AccountPage extends GenericPage {
 class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         title: const AppBarTitle("Account"),
       ),
-      body: const Center(
-        child: Icon(Icons.person)
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Container(                
+                  height: math.max(screenHeight, screenWidth) / 10,
+                  width: math.max(screenHeight, screenWidth) / 10,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/user_avatar.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 120),
+                Column(
+                  children: [
+                    Text('Rooms'),
+                    Text('12')
+                  ]
+                ),
+                SizedBox(width: 20),
+                Column(
+                  children: [
+                    Text('Devices'),
+                    Text('15'),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Text('HuyyCP', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                 backgroundColor: Colors.blue,
+                 foregroundColor: Colors.white,
+                 minimumSize: Size(double.infinity, 35)
+              ),
+              onPressed: () {}, 
+              child: Text('Edit user information')
+            )
+          ],
+        )
+      
       )
     );
   }
